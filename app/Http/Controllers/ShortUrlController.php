dab79891f619bab7037708c044c44e534076e759
Fabio Silva <fabinhoevil16@hotmail.com>
2024-03-11 21:16:37 -0300

    url should be unique

--- app/Http/Controllers/ShortUrlController.php
+++ app/Http/Controllers/ShortUrlController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Facades\Actions\CodeGenerator;
 use App\Models\ShortUrl;
+use Illuminate\Http\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class ShortUrlController extends Controller
 {
-    public function store()
+    public function store(): JsonResponse
     {
         request()->validate([
             'url' => 'required|url',
@@ -16,9 +17,11 @@ class ShortUrlController extends Controller
 
         $code = CodeGenerator::run();
 
-        $shortUrl = ShortUrl::query()->firstOrCreate([
-            'url' => request('url'),
-            'short_url' => config('api.url') . '/' . $code,
+        $shortUrl = ShortUrl::query()
+            ->firstOrCreate([
+            'url' => request('url')
+        ], [
+            'short_url' => config('app.url') . '/' . $code,
             'code' => $code,
         ]);
 
